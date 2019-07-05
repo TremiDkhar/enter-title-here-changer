813ff98562ad9c167a34bfd6f1abed9523e13bae
Tremi Dkhar <tremi@motiveinside.com>
2019-07-05 12:27:40 +0530

    Get Enter Title Here Changer to Work

--- admin/admin-pages.php
+++ admin/admin-pages.php
@@ -12,7 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) die(); // phpcs:ignore Generic.ControlStructures.I
 
 add_action( 'admin_init', 'ethc_register_settings' );
 function ethc_register_settings() {
-	register_setting( 'writing', 'ethc_options', 'intval' );
+	register_setting( 'writing', 'ethc_options' );
 	add_settings_section( 'ethc_sections', 'Enter Title Here Changer', 'ethc_settings_section_callback', 'writing' );
 	add_settings_field( 'ethc_new_title', 'Enter New Title', 'ethc_settings_field_callback', 'writing', 'ethc_sections', array( 'label_for' => 'ethc_new_title' ) );
 }
@@ -27,4 +27,14 @@ function ethc_settings_field_callback( $args ) {
 	?>
 	<input type="text" id="<?php echo esc_attr( $args['label_for'] ); ?>" name="ethc_options" value="<?php echo isset( $options ) ? esc_attr( $options ) : ''; ?>">
 	<?php
+}
+
+add_filter( 'enter_title_here', 'ethc_new_title' );
+function ethc_new_title( $title ) {
+	$options = get_option( 'ethc_options' );
+	if ( isset( $options ) ) {
+		$title = $options;
+	}
+
+	return $title;
 }
