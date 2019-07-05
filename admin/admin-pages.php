0ccc8aefbe498e6acf4da66588c1ada09ac041cd
Tremi Dkhar <tremi@motiveinside.com>
2019-07-05 07:59:17 +0530

    Add Option page based on WordPress Developer Handbook

--- admin/admin-pages.php
+++ admin/admin-pages.php
@@ -10,11 +10,21 @@
 
 if ( ! defined( 'ABSPATH' ) ) die(); // phpcs:ignore Generic.ControlStructures.InlineControlStructure.NotAllowed
 
-add_action( 'admin_menu', 'enter_title_here_changer_settings' );
-function enter_title_here_changer_settings() {
-	add_options_page( __( 'Enter Title Here Changer Settings', 'ethc' ), 'Title Changer', 'manage_options', 'ethc-settings', 'ethc_settings' );
+add_action( 'admin_init', 'ethc_register_settings' );
+function ethc_register_settings() {
+	register_setting( 'writing', 'ethc_options', 'intval' );
+	add_settings_section( 'ethc_sections', 'Enter Title Here Changer', 'ethc_settings_section_callback', 'writing' );
+	add_settings_field( 'ethc_new_title', 'Enter New Title', 'ethc_settings_field_callback', 'writing', 'ethc_sections', array( 'label_for' => 'ethc_new_title' ) );
 }
 
-function ethc_settings() {
-	echo 'Hello';
+
+function ethc_settings_section_callback( $args ) {
+	echo 'By default, the place holder of new post field is <kbd>Enter title here</kbd> or <kbd>Add title</kbd> in the latest WordPress. Using this settings will change to what title you want to display.';
+}
+
+function ethc_settings_field_callback( $args ) {
+	$options = get_option( 'ethc_options' );
+	?>
+	<input type="text" id="<?php echo esc_attr( $args['label_for'] ); ?>" name="ethc_options" value="<?php echo isset( $options ) ? esc_attr( $options ) : ''; ?>">
+	<?php
 }
