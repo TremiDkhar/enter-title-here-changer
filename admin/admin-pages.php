0a332548f2d78febf46666e68209818064da8bbb
Tremi Dkhar <tremi@motiveinside.com>
2019-07-06 10:49:38 +0530

    Add support for custom post type

--- admin/admin-pages.php
+++ admin/admin-pages.php
@@ -14,7 +14,11 @@ add_action( 'admin_init', 'ethc_register_settings' );
 function ethc_register_settings() {
 	register_setting( 'writing', 'ethc_options' );
 	add_settings_section( 'ethc_sections', 'Enter Title Here Changer', 'ethc_settings_section_callback', 'writing' );
-	add_settings_field( 'ethc_new_title', 'Enter New Title', 'ethc_settings_field_callback', 'writing', 'ethc_sections', array( 'label_for' => 'ethc_new_title' ) );
+
+	$posttypes = get_post_types();
+	foreach ( $posttypes as $posttype ) {
+		add_settings_field( 'eth_new_title_' . $posttype, $posttype, 'ethc_settings_field_callback', 'writing', 'ethc_sections', array( 'post_type' => $posttype ) );
+	}
 }
 
 
@@ -25,15 +29,19 @@ function ethc_settings_section_callback( $args ) {
 function ethc_settings_field_callback( $args ) {
 	$options = get_option( 'ethc_options' );
 	?>
-	<input type="text" id="<?php echo esc_attr( $args['label_for'] ); ?>" name="ethc_options" value="<?php echo isset( $options ) ? esc_attr( $options ) : ''; ?>">
+	<input type="text" id="<?php echo esc_attr( $args['label_for'] ); ?>" name="ethc_options[<?php echo esc_attr( $args['post_type'] ); ?>]" value="<?php echo $options[$args['post_type'] ]; ?>">
 	<?php
 }
 
 add_filter( 'enter_title_here', 'ethc_new_title' );
 function ethc_new_title( $title ) {
-	$options = get_option( 'ethc_options' );
-	if ( isset( $options ) ) {
-		$title = $options;
+
+	$screen  = get_current_screen();
+
+	$new_title = get_option( 'ethc_options' );
+
+	if ( isset( $new_title ) ) {
+		$title = $new_title;
 	}
 
 	return $title;
