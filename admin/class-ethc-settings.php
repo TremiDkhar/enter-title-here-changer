8423407809cba80d102e921c7a07ffe3a922a483
Tremi Dkhar <tremi@dkhar.com>
2020-09-12 18:56:24 +0530

    Comment unused code which are not required for the refactoring of the code

--- admin/class-ethc-settings.php
+++ admin/class-ethc-settings.php
@@ -40,9 +40,8 @@ class ETHC_Settings {
 	public function __construct() {
 
 		$this->settings = get_option( 'ethc_settings' );
-		add_action( 'admin_init', array( $this, 'register_settings' ) );
-		add_filter( 'enter_title_here', array( $this, 'new_title' ) );
-
+		add_action( 'admin_menu', array( $this, 'register_settings' ) );
+		// add_filter( 'enter_title_here', array( $this, 'new_title' ) );
 	}
 
 	/**
@@ -53,31 +52,32 @@ class ETHC_Settings {
 	 */
 	public function register_settings() {
 
-		register_setting( 'writing', 'ethc_settings', array( 'sanitize_callback' => array( $this, 'sanitize_data' ) ) );
-		add_settings_section( 'ethc_sections', 'Enter Title Here Changer - Settings', array( $this, 'settings_section_callback' ), 'writing' );
+		// register_setting( 'writing', 'ethc_settings', array( 'sanitize_callback' => array( $this, 'sanitize_data' ) ) );
+		add_submenu_page( 'options-general.php', 'Enter Title Here Changer Settings', 'ETHC Settings', 'manage_options', 'ethc-settings', array( $this, 'settings_page' ) );
+		// add_settings_section( 'ethc_sections', 'Enter Title Here Changer - Settings', array( $this, 'settings_section_callback' ), 'writing' );
 
-		$posttypes = get_post_types( '', 'object' );
+		// $posttypes = get_post_types( '', 'object' );
 
 		// Remove post types that are not required to change title.
-		unset( $posttypes['attachment'], $posttypes['revision'], $posttypes['nav_menu_item'], $posttypes['custom_css'], $posttypes['customize_changeset'], $posttypes['oembed_cache'], $posttypes['user_request'], $posttypes['wp_block'] );
+		// unset( $posttypes['attachment'], $posttypes['revision'], $posttypes['nav_menu_item'], $posttypes['custom_css'], $posttypes['customize_changeset'], $posttypes['oembed_cache'], $posttypes['user_request'], $posttypes['wp_block'] );
 
-		foreach ( $posttypes as $posttype ) {
+		// foreach ( $posttypes as $posttype ) {
 
-			$args = array(
-				'post_type' => $posttype->name,
-				'label_for' => $posttype->name,
-			);
+		// $args = array(
+		// 'post_type' => $posttype->name,
+		// 'label_for' => $posttype->name,
+		// );
 
-			add_settings_field( 'eth_new_title_' . $posttype->name, $posttype->labels->singular_name, array( $this, 'settings_field_callback' ), 'writing', 'ethc_sections', $args );
-		}
+		// add_settings_field( 'eth_new_title_' . $posttype->name, $posttype->labels->singular_name, array( $this, 'settings_field_callback' ), 'writing', 'ethc_sections', $args );
+		// }
 
-		add_settings_field( 'enter_new_title_uninstall_on_delete', 'Do you want to remove plugin data when plugin is removed?', array( $this, 'settings_uninstall' ), 'writing', 'ethc_sections', array( 'label_for' => 'ethc_settings_uninstall' ) );
+		// add_settings_field( 'enter_new_title_uninstall_on_delete', 'Do you want to remove plugin data when plugin is removed?', array( $this, 'settings_uninstall' ), 'writing', 'ethc_sections', array( 'label_for' => 'ethc_settings_uninstall' ) );
 	}
 
 	/**
 	 * Sanitize settings data before storing into database.
 	 *
-	 * @param array $settings | Raw settings data.
+	 * @param array $settings Raw settings data.
 	 * @return array
 	 */
 	public function sanitize_data( $settings ) {
@@ -97,6 +97,7 @@ class ETHC_Settings {
 	/**
 	 * Added Settings section
 	 *
+	 * @deprecated 0.4.0
 	 * @since 0.1.1
 	 * @return void
 	 */
@@ -105,9 +106,20 @@ class ETHC_Settings {
 	}
 
 	/**
+	 * Create plugin settings page
+	 *
+	 * @since 0.4.0
+	 * @return void
+	 */
+	public function settings_page() {
+		require_once ETHC_PATH . 'admin/ethc-settings-page.php';
+	}
+
+
+	/**
 	 * Add Settings fields for different custom post type.
 	 *
-	 * @param array $args | Argument needed for the input field.
+	 * @param array $args Argument needed for the input field.
 	 * @return void
 	 */
 	public function settings_field_callback( $args ) {
@@ -134,18 +146,18 @@ class ETHC_Settings {
 	 * @param string $title | The default placeholder.
 	 * @return string
 	 */
-	public function new_title( $title ) {
+	// public function new_title( $title ) {
 
-		$screen = get_current_screen()->post_type;
+	// $screen = get_current_screen()->post_type;
 
-		if ( ! empty( $screen ) ) {
-			$new_title = $this->settings;
-			if ( ! empty( $new_title[ $screen ] ) ) {
-				$title = $new_title[ $screen ];
-			}
-		}
+	// if ( ! empty( $screen ) ) {
+	// $new_title = $this->settings;
+	// if ( ! empty( $new_title[ $screen ] ) ) {
+	// $title = $new_title[ $screen ];
+	// }
+	// }
 
-		return $title;
-	}
+	// return $title;
+	// }
 
 }
