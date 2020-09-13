d517241940a0694b05af20c8b17033a794b1475f
Tremi Dkhar <tremi@dkhar.com>
2020-09-13 22:21:36 +0530

    Added the function `ethc_set_placeholder` plus documentation for some functions

--- admin/options.php
+++ admin/options.php
@@ -17,6 +17,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 /**
  * Retrive the Enter Title Here Changer plugin option from the database.
  *
+ * @since 0.4.0
  * @param string $key Option name.
  * @return string The value for the supplied $key in the database.
  */
@@ -39,6 +40,7 @@ function ethc_get_option( $key = null ) {
 /**
  * Retrived the saved editor title placeholder for the specified post type.
  *
+ * @since 0.4.0
  * @param string $post_type WordPress post type.
  * @return array Array contains label and the editor title place holder
  */
@@ -56,7 +58,42 @@ function ethc_get_placeholder( $post_type = null ) {
 	return $placeholder;
 }
 
+/**
+ * Set the Editor Title Placeholder for specified post type
+ *
+ * @since 0.4.0
+ * @param string $post_type The Post Type.
+ * @param string $placeholder New Editor Title Placeholder for the specified post type.
+ * @return void
+ */
+function ethc_set_placeholder( $post_type, $placeholder = '' ) {
 
+	// Exit if Post Type is not supplied.
+	if ( ! isset( $post_type ) ) {
+		return;
+	}
+
+	$new_placeholder = array();
+	$old_placeholder = get_option( 'ethc_placeholders' );
+	$post_object = get_post_type_object( $post_type );
+	$label = isset( $post_object ) ? $post_object->label : '';
+
+	$new_placeholder[ $post_type ] = array(
+		'label'		=> $label,
+		'placeholder'	=> $placeholder,
+	);
+
+	$placeholders = wp_parse_args( $new_placeholder, $old_placeholder );
+
+	update_option( 'ethc_placeholders',  $placeholders );
+}
+
+/**
+ * Retrive all the saved placeholder
+ *
+ * @since 0.4.0
+ * @return array $placeholders Array of all the placeholder
+ */
 function ethc_get_all_placeholder() {
 
 	$placeholders = get_option( 'ethc_placeholders' );
