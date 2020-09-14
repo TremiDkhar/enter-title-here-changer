1b206baf879f2d7a82b3772ef0985c54131eeb04
Tremi Dkhar <tremi@dkhar.com>
2020-09-14 19:50:42 +0530

    Removed unused methods

--- admin/class-ethc-settings.php
+++ admin/class-ethc-settings.php
@@ -50,59 +50,10 @@ class ETHC_Settings {
 	 * @since 0.1.1
 	 * @return void
 	 */
-	public function register_settings() {
+	public function register_setting() {
 
-		// register_setting( 'writing', 'ethc_settings', array( 'sanitize_callback' => array( $this, 'sanitize_data' ) ) );
 		add_submenu_page( 'options-general.php', 'Enter Title Here Changer Settings', 'ETHC Settings', 'manage_options', 'ethc-settings', array( $this, 'settings_page' ) );
-		// add_settings_section( 'ethc_sections', 'Enter Title Here Changer - Settings', array( $this, 'settings_section_callback' ), 'writing' );
 
-		// $posttypes = get_post_types( '', 'object' );
-
-		// Remove post types that are not required to change title.
-		// unset( $posttypes['attachment'], $posttypes['revision'], $posttypes['nav_menu_item'], $posttypes['custom_css'], $posttypes['customize_changeset'], $posttypes['oembed_cache'], $posttypes['user_request'], $posttypes['wp_block'] );
-
-		// foreach ( $posttypes as $posttype ) {
-
-		// $args = array(
-		// 'post_type' => $posttype->name,
-		// 'label_for' => $posttype->name,
-		// );
-
-		// add_settings_field( 'eth_new_title_' . $posttype->name, $posttype->labels->singular_name, array( $this, 'settings_field_callback' ), 'writing', 'ethc_sections', $args );
-		// }
-
-		// add_settings_field( 'enter_new_title_uninstall_on_delete', 'Do you want to remove plugin data when plugin is removed?', array( $this, 'settings_uninstall' ), 'writing', 'ethc_sections', array( 'label_for' => 'ethc_settings_uninstall' ) );
-	}
-
-	/**
-	 * Sanitize settings data before storing into database.
-	 *
-	 * @param array $settings Raw settings data.
-	 * @return array
-	 */
-	public function sanitize_data( $settings ) {
-
-		$uninstall_data = isset( $settings['uninstall_on_delete'] ) ? $settings['uninstall_on_delete'] : '';
-
-		foreach ( $settings as $setting => $value ) {
-			$settings[ $setting ] = sanitize_text_field( $value );
-		}
-
-		// Restore uninstall setting.
-		$settings['uninstall_on_delete'] = isset( $uninstall_data ) ? true : false;
-
-		return $settings;
-	}
-
-	/**
-	 * Added Settings section
-	 *
-	 * @deprecated 0.4.0
-	 * @since 0.1.1
-	 * @return void
-	 */
-	public function settings_section_callback() {
-		echo 'By default, the place holder of new post field is <kbd>Enter title here</kbd> or <kbd>Add title</kbd> in the latest WordPress. Using this settings will change to what title you want to display.';
 	}
 
 	/**
@@ -116,31 +67,6 @@ class ETHC_Settings {
 			require_once ETHC_PATH . 'admin/placeholder-action.php';
 		}
 		require_once ETHC_PATH . 'admin/ethc-settings-page.php';
-	}
-
-
-	/**
-	 * Add Settings fields for different custom post type.
-	 *
-	 * @param array $args Argument needed for the input field.
-	 * @return void
-	 */
-	public function settings_field_callback( $args ) {
-		?>
-		<input type="text" class="regular-text" id="<?php echo esc_attr( $args['label_for'] ); ?>" name="ethc_settings[<?php echo esc_attr( $args['post_type'] ); ?>]" value="<?php echo isset( $this->settings[ $args['post_type'] ] ) ? esc_attr( $this->settings[ $args['post_type'] ] ) : ''; ?>" />
-		<?php
-	}
-
-	/**
-	 * Delete plugin data on plugin uninstall?
-	 *
-	 * @param array $args | Argument needed for the input field.
-	 * @return void
-	 */
-	public function settings_uninstall( $args ) {
-		?>
-		<input type="checkbox" id="<?php echo esc_attr( $args['label_for'] ); ?>" name="ethc_settings[uninstall_on_delete]" <?php checked( true, isset( $this->settings['uninstall_on_delete'] ) ? $this->settings['uninstall_on_delete'] : false ); ?> />
-		<?php
 	}
 
 	/**
