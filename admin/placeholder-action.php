583d02eeac930c8d9958f214b4e9d11571978de5
Tremi Dkhar <tremi@dkhar.com>
2020-09-18 20:15:29 +0530

    Remove the function wraping to make the plugins action working

--- admin/placeholder-action.php
+++ admin/placeholder-action.php
@@ -1,9 +1,24 @@
 <?php
-if ( isset( $_POST['submit'] ) && 'Modify' === $_POST[ 'submit' ] ) {
+/**
+ * Perform the add, update and delete the place holder
+ *
+ * @package ETHC
+ * @subpackage Admin
+ * @author Tremi Dkhar
+ * @copyright Copyright (c) 2020, Tremi Dkhar
+ * @since 0.1.0
+ */
 
-	if ( isset( $_POST['post-type'] ) && isset( $_POST['placeholder'] ) ) {
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit();
+}
 
-		$post_type = sanitize_text_field( $_POST['post-type' ] );
+if ( isset( $_POST['submit'] ) && 'Modify' === $_POST['submit'] ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing -- Security check is not really necessary here
+
+	if ( isset( $_POST['post-type'] ) && isset( $_POST['placeholder'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing -- Security check is not really necessary here
+
+		$post_type   = sanitize_text_field( $_POST['post-type'] );
 		$placeholder = sanitize_text_field( $_POST['placeholder'] );
 
 		ethc_set_placeholder( $post_type, $placeholder );
