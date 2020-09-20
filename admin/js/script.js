63decbe20960cd4ff88a7a2724f19697ff9e53b7
Tremi Dkhar <tremi@dkhar.com>
2020-09-20 22:59:28 +0530

    Make JavaScript to handle the action links

--- admin/js/script.js
+++ admin/js/script.js
@@ -1,6 +1,24 @@
 "use strict";
 
 (function () {
-	var postType, placeHolder;
-	// postType = document.getElementById
+	var editLinks = document.querySelectorAll(".ethc-wrap .edit a");
+	var deleteLinks = document.querySelectorAll(".ethc-wrap .delete a");
+
+	editLinks.forEach((link) => {
+		link.addEventListener("click", handleEditLink);
+	});
+
+	deleteLinks.forEach((link) => {
+		link.addEventListener("click", handleDeleteLink);
+	});
+
+	function handleEditLink(e) {
+		console.log(e.target.getAttribute("data-post-type"));
+		e.preventDefault();
+	}
+
+	function handleDeleteLink(e) {
+		e.preventDefault();
+		console.log(e.target.getAttribute("data-post-type"));
+	}
 })();
