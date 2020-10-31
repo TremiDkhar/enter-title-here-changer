eeafdccda652d170cde8e436b0372012d8ab0329
Tremi Dkhar <tremi@dkhar.com>
2020-10-31 14:30:38 +0530

    Add SweetAlert before final delete the placeholder

--- admin/js/script.js
+++ admin/js/script.js
@@ -30,33 +30,47 @@
 	function handleDeleteLink(e) {
 		e.preventDefault();
 
-		const request = new XMLHttpRequest(),
-			postType = e.target.getAttribute("data-post-type"),
-			data = {
-				action: ETHC.action,
-				postType: postType,
-			};
+		swal({
+			title: 'Are you sure?',
+			text: 'Do you want to delete this placeholder?',
+			icon: 'warning',
+			buttons: {
+				cancel: true,
+				confirm: "Confirm",
+			}
+		}).then( (confirm ) => {
+			if ( confirm ) {
+				const request = new XMLHttpRequest(),
+				postType = e.target.getAttribute("data-post-type"),
+				data = {
+					action: ETHC.action,
+					postType: postType,
+				};
 
-		request.open("POST", ETHC.ajax_url, true);
-		request.setRequestHeader(
-			"Content-Type",
-			"application/x-www-form-urlencoded; charset=UTF-8"
-		);
+				request.open("POST", ETHC.ajax_url, true);
+				request.setRequestHeader(
+					"Content-Type",
+					"application/x-www-form-urlencoded; charset=UTF-8"
+				);
 
-		request.onload = function () {
-			console.log(request.response);
-		};
+				request.onload = function () {
+					console.log(request.response);
+				};
 
-		request.onerror = function () {
-			console.log("error");
-		};
+				request.onerror = function () {
+					console.log("error");
+				};
 
-		const query = `action=ethc_handle_delete_placeholder&post-type=${e.target.getAttribute(
-			"data-post-type"
-		)}&ethc-action=delete&ajax=1`;
-		console.log(query);
-		request.send(query);
+				const query = `action=ethc_handle_delete_placeholder&post-type=${e.target.getAttribute(
+					"data-post-type"
+				)}&ethc-action=delete&ajax=1`;
+				console.log(query);
+				request.send(query);
 
-		console.log(e.target.getAttribute("data-post-type"));
+				console.log(e.target.getAttribute("data-post-type"));
+			}
+		});
+
+
 	}
 })();
