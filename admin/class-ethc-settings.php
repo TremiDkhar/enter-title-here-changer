9e59e1a8fa0de383bc917c513299818f1f4b8351
Tremi Dkhar <tremi@dkhar.com>
2020-11-03 15:30:37 +0530

    Aligned array double arrow

--- admin/class-ethc-settings.php
+++ admin/class-ethc-settings.php
@@ -71,8 +71,8 @@ class ETHC_Settings {
 				'ethc',
 				'ETHC',
 				array(
-					'ajax_url' => admin_url( 'admin-ajax.php' ),
-					'ethc_nonce' => wp_create_nonce( 'ethc_placeholder_nonce' ),
+					'ajax_url'           => admin_url( 'admin-ajax.php' ),
+					'ethc_nonce'         => wp_create_nonce( 'ethc_placeholder_nonce' ),
 					'ethc_settings_page' => admin_url( 'options-general.php?page=ethc-settings' ),
 				)
 			);
